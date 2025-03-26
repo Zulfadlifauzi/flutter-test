67ba4d3360169db37210d6922bd4262c9b37bac4
Fauzi <fauzi_zulfadli@yahoo.com>
2025-03-27 02:42:32 +0800

    Add ProductShowScreen widget with basic Scaffold structure

---
+++ lib/src/product_module/presentation/product_show_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductShowScreen extends StatelessWidget {
+  const ProductShowScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
