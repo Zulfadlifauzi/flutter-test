7db59684b549c096125fdb6375b26d43a11012a6
Fauzi <fauzi_zulfadli@yahoo.com>
2025-03-27 01:12:09 +0800

    Add initial project setup with iOS and Android configurations, assets, and basic structure

---
+++ lib/src/product_module/presentation/product_index_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductIndexScreen extends StatelessWidget {
+  const ProductIndexScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Product Index Screen'),
+      ),
+    );
+  }
+}
